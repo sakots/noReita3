44ee25ab8e48c2df2db5b6b62f1beba876806bae
sakots <sakots8000@gmail.com>
2025-12-06 10:29:53 +0900

    feat: error関数

--- noreita3/functions.php
+++ noreita3/functions.php
@@ -6,6 +6,92 @@
 
 const FUNCTIONS_VER = 20251206;
 
+function error($str,$history_back=true): void {
+
+	global $en;
+
+	$async_flag = (bool)filter_input_data('POST','async_flag',FILTER_VALIDATE_BOOLEAN);
+	$http_x_requested_with = (bool)(isset($_SERVER['HTTP_X_REQUESTED_WITH']));
+	if($http_x_requested_with||$async_flag) {
+		header('Content-type: text/plain');
+		die(h("error\n{$str}"));
+	}
+
+	// あとで出力を修正
+	$templete='error.html';
+	include __DIR__.'/'.$skindir.$templete;
+	exit();
+}
+
+//csrfトークンを作成
+function get_csrf_token(): string {
+	session_sta();
+	$token = hash('sha256', session_id(), false);
+	$_SESSION['token'] = $token;
+
+	return $token;
+}
+//csrfトークンをチェッ
+function check_csrf_token(): void {
+	global $en;
+
+	if(($_SERVER["REQUEST_METHOD"]) !== "POST") {
+		error($en ? 'This operation has failed.' : '失敗しました。');
+	}
+	check_same_origin();
+	session_sta();
+	$token = (string)filter_input_data('POST','token');
+	$session_token = isset($_SESSION['token']) ? (string)$_SESSION['token'] : '';
+
+	if(!$token || !$session_token || !hash_equals($session_token,$token)) { //タイミング攻撃対策としてhash_equals()を使用
+		error($en ? "CSRF token mismatch.\nPlease reload." : "CSRFトークンが一致しません。\nリロードしてください。");
+	}
+}
+
+//session開始
+function session_sta(): void {
+	global $session_name;
+
+	$session_name = $session_name ?? 'session_petit';
+	$https_only = (bool)($_SERVER['HTTPS'] ?? '');
+
+	if(!isset($_SESSION)) {
+		ini_set('session.use_strict_mode', 1);
+		session_set_cookie_params(
+			0,"","",$https_only,true
+		);
+		session_name($session_name);
+		session_start();
+		header('Expires:');
+		header('Cache-Control:');
+		header('Pragma:');
+	}
+}
+
+function check_same_origin(): void {
+	global $en,$user_code;
+
+	session_sta();
+	$c_user_code = t(filter_input_data('COOKIE', 'user_code')); //user-codeを取得
+	$session_user_code = isset($_SESSION['user_code']) ? t($_SESSION['user_code']) : "";
+	if(!$c_user_code){
+		error($en ? 'Cookie check failed.' : 'Cookieが確認できません。');
+	}
+	if(!$user_code || ($user_code !== $c_user_code) && ($user_code !== $session_user_code)){
+		error($en ? "User code mismatch." : "ユーザーコードが一致しません。");
+	}
+
+	$sec_fetch_site = $_SERVER['HTTP_SEC_FETCH_SITE'] ?? '';
+	$same_origin = ($sec_fetch_site === 'same-origin');
+	
+	if(!isset($_SERVER['HTTP_ORIGIN']) || !isset($_SERVER['HTTP_HOST'])) {
+		error($en ? 'Your browser is not supported. ' : 'お使いのブラウザはサポートされていません。');
+	}
+	if(!$same_origin && (parse_url($_SERVER['HTTP_ORIGIN'], PHP_URL_HOST) !== $_SERVER['HTTP_HOST'])) {
+		error($en ? "The post has been rejected." : '拒絶されました。');
+	}
+}
+
 // ini_getで取得したサイズ文字列をMBに変換
 function ini_get_size_mb(string $key): float {
   if (!function_exists('ini_get')) return 0;
